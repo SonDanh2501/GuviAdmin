5e89b4dcc0a5c95355c57c33dcdb813718b4e7ba
Mac <mac@MacBook-Pro-cua-Mac.local>
2023-12-14 09:21:52 +0700

    add delete feedback

---
+++ src/pages/ManageFeedBack/ReviewCollaborator/components/DeleteModal/index.jsx
@@ -0,0 +1,49 @@
+import React, { memo, useState, useEffect } from 'react';
+import { Input, Modal } from 'antd';
+const { TextArea } = Input;
+const ModalDelete = (props) => {
+    const {
+        isShow,
+        value
+    } = props;
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    useEffect(() => {
+        console.log(value, 'value');
+        setIsModalOpen(isShow);
+        console.log(isShow, 'isShow');
+        console.log(isModalOpen, 'isModalOpen');
+    }, [isShow])
+
+    const showModal = () => {
+        setIsModalOpen(true);
+      };
+    
+      const handleOk = () => {
+        setIsModalOpen(false);
+        if(props.detectClose) {
+          props.detectClose("")
+        }
+      };
+    
+      const handleCancel = () => {
+        setIsModalOpen(false);
+        if(props.detectClose) {
+          props.detectClose("")
+        }
+      };
+
+
+    return (
+        <>
+      <Modal title="Xác nhận xoá" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+        <p>Bạn có chắc muốn xoá phản hồi đơn </p>
+      <TextArea rows={4} value={value} />
+      </Modal>
+        </>
+    )
+
+
+}
+
+export default memo(ModalDelete);
