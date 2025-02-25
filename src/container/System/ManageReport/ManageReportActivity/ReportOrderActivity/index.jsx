ad687276b35b95bdfd3be88a7dcf951adb9ec9a6
Son <sondanhwork2501@gmail.com>
2025-02-25 10:48:19 +0700

    export excel file

--- src/container/System/ManageReport/ManageReportActivity/ReportOrderActivity/index.jsx
+++ src/container/System/ManageReport/ManageReportActivity/ReportOrderActivity/index.jsx
@@ -26,6 +26,8 @@ import {
 } from "recharts";
 import { number_processing } from "../../../../../helper/numberProcessing";
 import { CaretDownOutlined, CaretUpOutlined } from "@ant-design/icons";
+import ButtonCustom from "../../../../../components/button";
+import { exportToExcel } from "../../../../../utils/contant";
 
 const { IoReceipt, IoCash, IoTrendingUp, IoHappy } = icons;
 
@@ -448,6 +450,19 @@ const ReportOrderActivity = () => {
       </div>
     );
   };
+
+  const leftContent = () => {
+    return (
+      <div>
+        <ButtonCustom
+          label="Xuất Excel"
+          customColor="green"
+          onClick={() => exportToExcel(listData, "Bao_cao_hoat_dong_don_hang")}
+        />
+      </div>
+    );
+  };
+
   /* ~~~ Main ~~~ */
   return (
     <div className="report-order-daily-revenue">
@@ -539,6 +554,7 @@ const ReportOrderActivity = () => {
               moment(previousStartDate).format("DD/MM/YYYY"),
               moment(previousEndDate).format("DD/MM/YYYY")
             )}
+            leftContent={leftContent()}
           />
         </div>
       </div>
